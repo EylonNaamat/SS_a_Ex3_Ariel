bab237e59bfd2bdd6c771e24294fdcb3ec662b12
michael <you@example.com>
2021-12-16 23:38:09 +0200

    second try

--- functions.c
+++ functions.c
@@ -6,49 +6,49 @@ void gematrias(char *word, char *text)
     int geo = checkwordgeo(word);
     while(*text!='\0')
     {
-        if((*word =! '\n') && (*word != '\t') &&(*word != ' '))
+        if((*text != '\n') && (*text != '\t') &&(*text != ' '))
         {
-            checkWordGeo(word);
+            checkFromGeo(text,geo);
         }
-        word++;
+        text++;
     }
 }
 
-int checkWordGeo(char* word)
+int checkWordGeo(char *word)
 {
     int sum=0;
-    while(*word == '\0')
+    while(*word != '\0')
     {
         if(65<=*word<=90)
         {
-            sum = sum + *word -65;
+            sum = sum + *word -64;
         }
         if(97<=*word<=122)
         {
-            sum = sum + *word -97;  
+            sum = sum + *word -96;  
         }
         word++;
     } 
+    return sum;
 }
 
-int checkFromGeo(char* word, int needSum)
+void checkFromGeo(char *text, int needSum)
 {
     int sum=0;
-    char* start= word;
+    char* start= text;
     int count = 0;
-    while((sum<needSum)&&(*word!='\0') )
+    while((sum<needSum)&&(*text!='\0') )
     {
-        if(65<=*word<=90)
+        if(65<=*text<=90)
         {
-            sum = sum + *word -65;
-            count++;
+            sum = sum + *text -64;
         }
-        if(97<=*word<=122)
+        if(97<=*text<=122)
         {
-            sum = sum + *word -97;
-            count++;
+            sum = sum + *text -96;
         }
-        word++;
+        text++;
+        count++;
     }
     if(sum == needSum)
     {
@@ -61,6 +61,70 @@ int checkFromGeo(char* word, int needSum)
     }
 }
 
-void atbash(char* word, char* text){
-    char atbashWord [WORD];
+void anagram(char *word,char *text)
+{
+    int chars[256]={0};
+    int length = fullArrAndlength(word,chars);
+    int copychars[256];
+    while(*text!='\0')
+    {
+        if((*text != '\n') && (*text != '\t') &&(*text != ' '))
+        {
+            myCopyArr(chars,copychars);
+            checkFromAnag(text,copychars,length);
+
+        }
+    }
 }
+
+int fullArrAndlength(char *word,int chars[])
+{
+    int sum = 0;
+    while(*word != '\0')
+    {
+        chars[*word]++;
+        sum++;
+    }
+}
+void myCopyArr(int src[],int dest[])
+{
+    for(int i=0 ; i<256 ; i++)
+    {
+        dest[i]=src[i];
+    }
+}
+void checkFromAnag(char *text, int coChars[], int length)
+{
+    char *start = text;
+    int count =0;
+    while ((*text !='\0')&&(length>0))
+    {
+        if((*text != '\n') && (*text != '\t') &&(*text != ' '))
+        {
+            coChars[*text]--;
+            length--;
+        }
+        text++;
+        count++;
+    }
+    if(length!=0)
+    {
+        return;
+    }
+    for(int i=0;i<256;i++)
+    {
+        if(coChars[i]!=0)
+        {
+            return;
+        }
+        coChars[i]=0;
+    }
+    while(count!=0)
+    {
+        print("%c", *start);
+        start++;
+        count--;
+    }
+    
+}
+
---
+++ makefile
@@ -0,0 +1,18 @@
+functions.o : functions.c functions.h
+	gcc -c -Wall functions.c
+
+libfunction.a : functions.o functions.h
+	ar rcu libfunction.a functions.o
+
+main.o : main.c functions.h
+	gcc -c -Wall main.c
+
+stringProg : main.o libfunction.a
+	gcc -Wall main.o ./libfunction.a -o stringProg
+
+.PHONY: clean
+
+clean:
+	rm *.o *.a stringProg
+
+all : stringProg
